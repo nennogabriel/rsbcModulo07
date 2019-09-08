6db7d87a893035d3af199ff483a4c2c4237b5145
Pedro Nascimento <nennogabriel@gmail.com>
2019-09-08 12:24:17 -0300

    07a15 Remover produto

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -15,6 +15,13 @@ export default function cart(state = [], action) {
           });
         }
       });
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const producIndex = draft.findIndex(p => p.id === action.id);
+        if (producIndex >= 0) {
+          draft.splice(producIndex, 1);
+        }
+      });
     default:
       return state;
   }
